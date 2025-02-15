dfef45791ceb7192ca052a4a5ee7411c6526b5ba
Abdel1117 <abderahmane.adjali@live.fr>
2025-02-15 13:31:58 +0100

    resolving unit test

--- src/__tests__/BlockTest/BlockTest.test.tsx
+++ src/__tests__/BlockTest/BlockTest.test.tsx
@@ -1,46 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect, vi } from 'vitest';
-import { Block } from '../../components/Block/Block';
-import Wargame from '../../assets/images/Wargame.jpg';
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { Block } from "../../components/Block/Block";
+import Wargame from "../../assets/images/Wargame.jpg";
 
-
-describe('Accueil Component', () => {
-
-it('should render the Block component with the correct props', () => {
+describe("Accueil Component", () => {
+  it("should render the Block component with the correct props", () => {
     const mockFunction = vi.fn();
     render(
-      <Block 
-        image={Wargame} 
+      <Block
+        image={Wargame}
         textAlt={"Mon image"}
         title={"Notre site"}
-        text={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quibusdam quia, architecto similique alias, harum debitis non tempora porro asperiores explicabo vel recusandae voluptates dignissimos mollitia saepe sequi tempore quos hic? Aliquid, consequuntur? Laborum quos eius ipsa ea consequuntur repudiandae."}
+        text={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quibusdam quia, architecto similique alias, harum debitis non tempora porro asperiores explicabo vel recusandae voluptates dignissimos mollitia saepe sequi tempore quos hic? Aliquid, consequuntur? Laborum quos eius ipsa ea consequuntur repudiandae."
+        }
         callToActionMessage={"Cliquer ici"}
         imageOrder={1}
         SpeechOrder={2}
-        actionButton={mockFunction} 
+        actionButton={mockFunction}
       />
     );
-// Check if the image is rendered
-const image = screen.getByAltText('Mon image');
-expect(image).toBeInTheDocument();
-expect(image).toHaveAttribute('src', Wargame);
+    // Check if the image is rendered
+    const image = screen.getByAltText("Mon image");
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", Wargame);
 
-// Check if the title is rendered
-const title = screen.getByText('Notre site');
-expect(title).toBeInTheDocument();
+    // Check if the title is rendered
+    const title = screen.getByText("Notre site");
+    expect(title).toBeInTheDocument();
 
-// Check if the text is rendered
-const text = screen.getByText(/Lorem ipsum dolor sit, amet consectetur/i);
-expect(text).toBeInTheDocument();
+    // Check if the text is rendered
+    const text = screen.getByText(/Lorem ipsum dolor sit, amet consectetur/i);
+    expect(text).toBeInTheDocument();
 
-// Check if the call to action message is rendered
-const callToAction = screen.getByText('Cliquer ici');
-expect(callToAction).toBeInTheDocument();
-
-// Mock the function and check if it is called
+    // Check if the call to action message is rendered
+    const callToAction = screen.getAllByText("Cliquer ici");
+    expect(callToAction).toHaveLength(2);
+    // Mock the function and check if it is called
     // Simulate button click
-    const button = screen.getByText('Cliquer ici');
-    button.click();
+    const button = screen.getAllByText("Cliquer ici");
+    button[0].click();
+    button[1].click();
+
     expect(mockFunction).toHaveBeenCalled();
   });
-})
+});
--- src/__tests__/HomeTest/HomeTest.test.tsx
+++ src/__tests__/HomeTest/HomeTest.test.tsx
@@ -1,14 +1,38 @@
 // Accueil.test.tsx
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect  } from 'vitest';
-import Accueil from '../../pages/Accueil/Accueil';
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import Accueil from "../../pages/Accueil/Accueil";
+import { useAppSelector } from "../../app/hooks";
 
-describe('Accueil Component', () => {
-    beforeEach(()=>{
-        render(<Accueil />)
-      })
+// On mocke le hook useThemeContext
+vi.mock("../../context/useThemeContext", () => {
+  return {
+    default: () => ({
+      theme: "light",
+      toggleTheme: vi.fn(),
+    }),
+  };
+});
+
+/* On mock le useNavigate */
+vi.mock("react-router-dom", () => ({
+  useNavigate: () => vi.fn(),
+}));
+vi.mock("../../app/hooks.ts", async () => {
+  const actual = await vi.importActual("../../app/hooks.ts");
+  return {
+    ...actual,
+    useAppDispatch: () => vi.fn(),
+    useAppSelector: () => vi.fn(),
+  };
+});
+
+describe("Accueil Component", () => {
+  beforeEach(() => {
+    render(<Accueil />);
+  });
   it("Should render the homePage", () => {
-      const title = screen.getByTestId("HomeApp")
-      expect(title).toBeInTheDocument()
-  })
+    const title = screen.getByTestId("HomeApp");
+    expect(title).toBeInTheDocument();
+  });
 });
